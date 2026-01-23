3a9447c2f4bce82e0e97932478ed560d023617b6
oumar-abdallah-abakar <abukr.campus@gmail.com>
2026-01-23 13:49:42 +0100

    feat: premier exemple

---
+++ passage_valeur_ou_reference/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        var entier = 5;
+        mofify(entier);
+        System.out.println(entier);
+    }
+
+    public static void mofify(int entier) {
+        entier = 10;
+    }
+
+}
