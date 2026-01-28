a319bd4a40eed8a43a494c3c5e9471cf5e5f9899
oumar-abdallah-abakar <abukr.campus@gmail.com>
2026-01-28 15:31:32 +0100

    les-attributs-&-le-constructeur

--- POO/src/Fruit.java
+++ POO/src/Fruit.java
@@ -1,3 +1,13 @@
 public class Fruit {
 
+    public String nom;
+    public String couleur;
+    public String origine;
+
+    public Fruit(String nom, String couleur, String origine) {
+        this.nom = nom;
+        this.couleur = couleur;
+        this.origine = origine;
+    }
+
 }
--- POO/src/Main.java
+++ POO/src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        Fruit fruit = new Fruit();
-        Fruit fuit2 = new Fruit();
+        Fruit fruit = new Fruit("Faise", "Rouge", "Tchad");
+        Fruit fuit2 = new Fruit("Pomme", "Vert", "Cameroun");
+        System.out.println("Fruit : " + fruit.nom );
+        System.out.println("Fruit : " + fuit2.nom);
 
     }
 }
