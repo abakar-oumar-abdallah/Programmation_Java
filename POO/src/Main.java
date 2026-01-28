8b333e23d194a28ccb6406582713832beb044fb5
oumar-abdallah-abakar <abukr.campus@gmail.com>
2026-01-28 15:13:02 +0100

    notions-des-cours-sur-classes

--- POO/src/Main.java
+++ POO/src/Main.java
@@ -6,3 +6,19 @@ public class Main {
 
     }
 }
+
+/*
+
+Les classes :
+
+En Java, il est interdit de définir plusieurs classes différentes dans le même fichier source.
+Il faut donc créer autant de fichier .java que de classe dans le code (à l'exception des classes internes que nous
+verrons plus tard dans le chapitre).
+
+Le nom d'une classe doit respecter la convention de nommage du PascalCase
+(toutes les premières lettres de chaque mot doivent être en majuscule).
+
+Les objets :
+
+Pour rappel, un objet représente une instance d'une classe.
+ */
