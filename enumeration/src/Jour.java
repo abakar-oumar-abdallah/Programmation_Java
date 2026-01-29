8cc6782750764d7a42fb002b0169b72b22b92ed6
oumar-abdallah-abakar <abukr.campus@gmail.com>
2026-01-29 17:16:36 +0100

    éeeumenumeration

---
+++ enumeration/src/Jour.java
@@ -0,0 +1,13 @@
+public enum Jour {
+    Lundi, Mardi, Mercredi, Jeudi, Vendredi, Samedi, Dimanche
+}
+
+/*
+Créer une énumération :
+
+Les énumérations sont un type de données que l'on peut utiliser en Java pour représenter une liste de données,
+comme le montre l'exemple ci-dessus.
+
+L'énumération Jour permet ici de lister l'ensemble des jours de la semaine. Sa visibilité doit être précisée et
+le mot-clé enum permet de la définir.
+ */
---
+++ enumeration/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Jour jour = Jour.Dimanche;
+
+        System.out.println(jour);
+
+    }
+}
